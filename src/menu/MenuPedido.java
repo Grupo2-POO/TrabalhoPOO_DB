e2672dfadc83db31c9418c25c313e6453abdafd2
jcardosodev <jonathanpcardoso24@gmail.com>
2024-04-10 12:14:00 -0700

    Merge branch 'main' of https://github.com/Grupo2-POO/TrabalhoPOO_DB.git into jonathan

--- src/menu/MenuPedido.java
+++ src/menu/MenuPedido.java
@@ -14,9 +14,9 @@ public final class MenuPedido extends NossoMenu {
 	@Override
 	public void processarOpcao(int opcao) {
 		switch (opcao) {
-		case 1: Util.printMessage("Cadastro de pedidos vai rolar aqui"); break;
-		case 2: Util.printMessage("Consulta de pedidos vai rolar aqui"); break;
-		case 3: Util.printMessage("Alteração de pedidos vai rolar aqui"); break; 
+		case 1: cadastrarPedidos(); break;
+		case 2: consultarPedidos(); break;
+		case 3: alterarPedidos(); break; 
 		case 4: {
 				Util.printMessage("Voltando ao menu inicial de pedidos vai rolar aqui"); 
 				// executa o menu inicial
@@ -26,6 +26,18 @@ public final class MenuPedido extends NossoMenu {
 				break; 
 			}
 		}
+		
 	}
-
+	
+	public void cadastrarPedidos() {
+		
+		
+	}
+	public void consultarPedidos() {
+		
+	}
+	
+	public void alterarPedidos() {
+		
+	}
 }
